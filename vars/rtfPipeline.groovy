1a579664f66e7d8413988a944e3551a36f76da67
sudhish_s <saksnair2013@gmail.com>
2023-02-14 21:41:33 -0500

    Fixed the String Interpolation issue

--- vars/rtfPipeline.groovy
+++ vars/rtfPipeline.groovy
@@ -7,32 +7,25 @@ def call(Map pipelineParams) {
     }
 
     environment {
-      //Anypoint Platform connected app credentials (clientId/secret) - appName: ci-cd-connected-app
-      ANYPOINT_CONNECTED_APP_CREDENTIALS = credentials("anypoint-deploy")
 
       //Maven settings XML
-      MAVEN_SETTINGS_XML=credentials('mvn-settings')
+      // MAVEN_SETTINGS_XML=credentials('mvn-settings')
 
-      // PORTFOLIO_NAME=${pipelineParams.portfolio}
-      // PORTFOLIO_NAME_LOWER="${PORTFOLIO_NAME.toLowerCase()}"   
+      PORTFOLIO_NAME=${pipelineParams.portfolio}
+      PORTFOLIO_NAME_LOWER="${PORTFOLIO_NAME.toLowerCase()}"       
 
-      CONNECTED_APP_CLIENT_ID = "${ANYPOINT_CONNECTED_APP_CREDENTIALS_USER}"
-      CONNECTED_APP_CLIENT_SECRET = "${ANYPOINT_CONNECTED_APP_CREDENTIALS_PSW}"        
+      MULE_VERSION = mwDefaults.deployment_Params_Defaults.muleVersion
+      RTF_PROVIDER = mwDefaults.deployment_Params_Defaults.provider
+      SKIP_DEPLOY_VERIFY = mwDefaults.deployment_Params_Defaults.skipDeployVerification
 
-      // MULE_VERSION = mwDefaults.deployment_Params_Defaults.muleVersion
-      // RTF_PROVIDER = mwDefaults.deployment_Params_Defaults.provider
-      // SKIP_DEPLOY_VERIFY = mwDefaults.deployment_Params_Defaults.skipDeployVerification
+      ENFORCE_REPLICAS_ACROSS_NODES = mwDefaults.deployment_Params_Defaults.enforceReplicasAcrossNodes
+      UPDATESTRATEGY = mwDefaults.deployment_Params_Defaults.updateStrategy
+      CLUSTERED = mwDefaults.deployment_Params_Defaults.clustered
+      FORWARD_SSL_SESSION = mwDefaults.deployment_Params_Defaults.forwardSSLSession
+      LAST_MILE_SECURITY = mwDefaults.deployment_Params_Defaults.lastMileSecurity
+      PERSISTENT_OBJECT_STORE = mwDefaults.deployment_Params_Defaults.persistentObjectStore
 
-
-
-      // ENFORCE_REPLICAS_ACROSS_NODES = mwDefaults.deployment_Params_Defaults.enforceReplicasAcrossNodes
-      // UPDATESTRATEGY = mwDefaults.deployment_Params_Defaults.updateStrategy
-      // CLUSTERED = mwDefaults.deployment_Params_Defaults.clustered
-      // FORWARD_SSL_SESSION = mwDefaults.deployment_Params_Defaults.forwardSSLSession
-      // LAST_MILE_SECURITY = mwDefaults.deployment_Params_Defaults.lastMileSecurity
-      // PERSISTENT_OBJECT_STORE = mwDefaults.deployment_Params_Defaults.persistentObjectStore
-
-      // ANYPOINT_URL = mwDefauls.deployment_Params_Defaults.anypoint_url
+      ANYPOINT_URL = mwDefauls.deployment_Params_Defaults.anypoint_url
 
       // ANYPOINT_URL = "https://anypoint.mulesoft.com"
       // MULE_VERSION =  "4.4.0"
@@ -50,14 +43,16 @@ def call(Map pipelineParams) {
       // IS_PRODUCTION = "false"
     }
 
-    //def appName = "${PORTFOLIO_NAME_LOWER}-${pipelineParams.projectName}-${ANYPOINT_DEV}"
+    
 
     stages {
 
       stage('Compile') {        
         steps {
           echo "Branch -- ${env.GIT_BRANCH}"
-          sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U clean compile"
+          withCredentials([file(credentialsId: "mvn-settings", variable: 'MAVEN_SETTINGS_XML')]) {
+              sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U clean compile"
+          }
         }    
       }
 
@@ -66,51 +61,55 @@ def call(Map pipelineParams) {
           expression { "${RUN_TESTS}" == 'true' }
         }
         steps {
-          sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U test"
+          withCredentials([file(credentialsId: "mvn-settings", variable: 'MAVEN_SETTINGS_XML')]) {
+            sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U test"
+          }
         }
       }
 
       stage('Publish to Exchange') {
         steps {
           script {
-             sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U -PExchange deploy -DskipTests"
+            withCredentials([file(credentialsId: "mvn-settings", variable: 'MAVEN_SETTINGS_XML')]) {
+              sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U -PExchange deploy -DskipTests"
+            }
           }
         }
       }
 
-      // stage('Deploy to RTF Dev1') {
-      //   when {
-      //     allOf { 
-      //       expression { "${env.GIT_BRANCH}" == 'develop' }
-      //       expression { "${IS_PRODUCTION}" == 'false' }
-      //     }
-      //   }
-      //   environment {
-      //     MULE_ENV="dev1"
-      //     ANYPOINT_ENV=${MULE_ENV}
+      stage('Deploy to RTF Dev1') {
+        when {
+          allOf { 
+            expression { "${env.GIT_BRANCH}" == 'develop' }
+            expression { "${IS_PRODUCTION}" == 'false' }
+          }
+        }
+        environment {
+          MULE_ENV="dev1"
+          ANYPOINT_ENV=${MULE_ENV}
 
-      //     PORTFOLIO_CREDENTIALS = credentials("${PORTFOLIO_NAME_LOWER}-anypoint-${MULE_ENV}")
+          RTF_CLUSTER_NAME = mwDefauls.portFolio_Env_Mappings["${PORTFOLIO_NAME}"]["${MULE_ENV}"]
 
-      //     SECRET_ENV_KEY=credentials("${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-key")          
-      //     ANYPOINT_ENV_CLIENT_ID = "${PORTFOLIO_CREDENTIALS_USER}"
-      //     ANYPOINT_ENV_CLIENT_SECRET = "${PORTFOLIO_CREDENTIALS_PSW}"
+          CPU_RESERVED = mwDefaults.DEV1_Resource_Defaults.cpu_reserved
+          CPU_LIMIT = mwDefaults.DEV1_Resource_Defaults.cpu_limit
+          MEMORY_RESERVED = mwDefaults.DEV1_Resource_Defaults.memory_reserved
+          REPLICAS = mwDefaults.DEV1_Resource_Defaults.replicas                  
+        }
 
-      //     RTF_CLUSTER_NAME = mwDefauls.portFolio_Env_Mappings["${PORTFOLIO_NAME}"]["${MULE_ENV}"]
-
-      //     CPU_RESERVED = mwDefaults.DEV1_Resource_Defaults.cpu_reserved
-      //     CPU_LIMIT = mwDefaults.DEV1_Resource_Defaults.cpu_limit
-      //     MEMORY_RESERVED = mwDefaults.DEV1_Resource_Defaults.memory_reserved
-      //     REPLICAS = mwDefaults.DEV1_Resource_Defaults.replicas
-                  
-      //   }
-
-      //   steps {
-      //     script { 
-      //       print "App Name: " + appName         
-      //       sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U mule:deploy -Dmule.artifact=myArtifact.jar -Dmule.app.name=${appName}"
-      //     }
-      //   }
-      // }
+        steps {
+          script {
+            def appName = "${PORTFOLIO_NAME_LOWER}-${pipelineParams.projectName}-${ANYPOINT_DEV}"
+            print "App Name: " + appName
+            withCredentials(
+              [file(credentialsId: "mvn-settings", variable: 'MAVEN_SETTINGS_XML')],
+              [usernamePassword(credentialsId: "${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-key", usernameVariable: 'AP_USER', passwordVariable: 'AP_PASS')],
+              [string(credentialsId: "${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-key", variable: 'key')]
+              ) {
+                sh "mvn -s ${MAVEN_SETTINGS_XML} -B -U mule:deploy -Dmule.artifact=dummy.jar -Dmule.app.name=${appName} -Danypoint.env.clientId=${ap_user} -Danypoint.env.clientSecret=${ap_pass} -Dsecret.key=${key}"
+            }                     
+          }
+        }
+      }
 
     }
   }
