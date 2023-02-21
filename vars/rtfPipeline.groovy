497f3197813808e2b31fddf428be7355c73603e3
sudhish_s <saksnair2013@gmail.com>
2023-02-21 16:53:31 -0500

    used withEnv

--- vars/rtfPipeline.groovy
+++ vars/rtfPipeline.groovy
@@ -60,73 +60,76 @@ def call(Map pipelineParams) {
             expression { "${IS_PRODUCTION}" == 'false' }
           }
         }
-        script {
-          def deployParams = mwDefaults.deployment_Params_Defaults
+        // script {
+        //   def deployParams = mwDefaults.deployment_Params_Defaults
 
-          println "deployParams1: ${deployParams}"
+        //   println "deployParams1: ${deployParams}"
 
-          deployUtils.getDeploymentConfigs(deployParams,"${PORTFOLIO_NAME_LOWER}","${ANYPOINT_ENV}","${PROJECT}")
+        //   deployUtils.getDeploymentConfigs(deployParams,"${PORTFOLIO_NAME_LOWER}","${ANYPOINT_ENV}","${PROJECT}")
 
-          println "deployParams2: ${deployParams}"
-        }
-        environment {
-          MULE_ENV="dev1"
-          ANYPOINT_ENV="${MULE_ENV}"
+        //   println "deployParams2: ${deployParams}"
+        // }
+        // environment {
+        //   MULE_ENV="dev1"
+        //   ANYPOINT_ENV="${MULE_ENV}"
 
-          //PORTFOLIO_ENV = "${mwDefaults.portFolio_Env_Mappings[PORTFOLIO_NAME]}"
+        //   //PORTFOLIO_ENV = "${mwDefaults.portFolio_Env_Mappings[PORTFOLIO_NAME]}"
 
-          // DEPLOY_PARAMS="${deployUtils.getDeploymentConfigs(mwDefaults.deployment_Params_Defaults, 
-          //           mwDefaults.deployment_Params_Constants,PORTFOLIO_NAME_LOWER,ANYPOINT_DEV,PROJECT)}"
+        //   DEPLOY_PARAMS="${deployUtils.getDeploymentConfigs(mwDefaults.deployment_Params_Defaults, 
+        //             PORTFOLIO_NAME_LOWER,ANYPOINT_DEV,PROJECT)}"
           
  
-          // RTF_CLUSTER_NAME = "${mwDefaults.portFolio_Env_Mappings[PORTFOLIO_NAME][MULE_ENV][0]}"
+        //   // RTF_CLUSTER_NAME = "${mwDefaults.portFolio_Env_Mappings[PORTFOLIO_NAME][MULE_ENV][0]}"
 
-          // CPU_RESERVED = "${mwDefaults.DEV1_Resource_Defaults.cpu_reserved}"
-          // CPU_LIMIT = "${mwDefaults.DEV1_Resource_Defaults.cpu_limit}"
-          // MEMORY_RESERVED = "${mwDefaults.DEV1_Resource_Defaults.memory_reserved}"
-          // REPLICAS = "${mwDefaults.DEV1_Resource_Defaults.replicas}"  
+        //   // CPU_RESERVED = "${mwDefaults.DEV1_Resource_Defaults.cpu_reserved}"
+        //   // CPU_LIMIT = "${mwDefaults.DEV1_Resource_Defaults.cpu_limit}"
+        //   // MEMORY_RESERVED = "${mwDefaults.DEV1_Resource_Defaults.memory_reserved}"
+        //   // REPLICAS = "${mwDefaults.DEV1_Resource_Defaults.replicas}"  
 
-          // MULE_VERSION = "${mwDefaults.deployment_Params_Defaults.muleVersion}"
-          // RTF_PROVIDER = "${mwDefaults.deployment_Params_Defaults.provider}"
-          // SKIP_DEPLOY_VERIFY = "${mwDefaults.deployment_Params_Defaults.skipDeployVerification}"
+        //   // MULE_VERSION = "${mwDefaults.deployment_Params_Defaults.muleVersion}"
+        //   // RTF_PROVIDER = "${mwDefaults.deployment_Params_Defaults.provider}"
+        //   // SKIP_DEPLOY_VERIFY = "${mwDefaults.deployment_Params_Defaults.skipDeployVerification}"
 
-          // ENFORCE_REPLICAS_ACROSS_NODES = "${mwDefaults.deployment_Params_Defaults.enforceReplicasAcrossNodes}"
-          // UPDATE_STRATEGY = "${mwDefaults.deployment_Params_Defaults.updateStrategy}"
-          // CLUSTERED = "${mwDefaults.deployment_Params_Defaults.clustered}"
-          // FORWARD_SSL_SESSION = "${mwDefaults.deployment_Params_Defaults.forwardSSLSession}"
-          // LAST_MILE_SECURITY = "${mwDefaults.deployment_Params_Defaults.lastMileSecurity}"
-          // PERSISTENT_OBJECT_STORE = "${mwDefaults.deployment_Params_Defaults.persistentObjectStore}"     
+        //   // ENFORCE_REPLICAS_ACROSS_NODES = "${mwDefaults.deployment_Params_Defaults.enforceReplicasAcrossNodes}"
+        //   // UPDATE_STRATEGY = "${mwDefaults.deployment_Params_Defaults.updateStrategy}"
+        //   // CLUSTERED = "${mwDefaults.deployment_Params_Defaults.clustered}"
+        //   // FORWARD_SSL_SESSION = "${mwDefaults.deployment_Params_Defaults.forwardSSLSession}"
+        //   // LAST_MILE_SECURITY = "${mwDefaults.deployment_Params_Defaults.lastMileSecurity}"
+        //   // PERSISTENT_OBJECT_STORE = "${mwDefaults.deployment_Params_Defaults.persistentObjectStore}"     
 
-          CPU_RESERVED = "${deployParams.cpu_reserved}"
-          CPU_LIMIT = "${deployParams.cpu_limit}"
-          MEMORY_RESERVED = "${deployParams.memory_reserved}"
-          REPLICAS = "${deployParams.replicas}"  
+        //   CPU_RESERVED = "${deployParams.cpu_reserved}"
+        //   CPU_LIMIT = "${deployParams.cpu_limit}"
+        //   MEMORY_RESERVED = "${deployParams.memory_reserved}"
+        //   REPLICAS = "${deployParams.replicas}"  
 
-          MULE_VERSION = "${deployParams.mule_version}"
-          RTF_PROVIDER = "${deployParams.provider}"
-          SKIP_DEPLOY_VERIFY = "${deployParams.skip_deploy_verify}"
+        //   MULE_VERSION = "${deployParams.mule_version}"
+        //   RTF_PROVIDER = "${deployParams.provider}"
+        //   SKIP_DEPLOY_VERIFY = "${deployParams.skip_deploy_verify}"
 
-          ENFORCE_REPLICAS_ACROSS_NODES = "${deployParams.enforce_replicas_across_nodes}"
-          UPDATE_STRATEGY = "${deployParams.update_strategy}"
-          CLUSTERED = "${deployParams.clustered}"
-          FORWARD_SSL_SESSION = "${deployParams.forward_ssl_session}"
-          LAST_MILE_SECURITY = "${deployParams.last_mile_security}"
-          PERSISTENT_OBJECT_STORE = "${deployParams.persistent_object_store}"                  
+        //   ENFORCE_REPLICAS_ACROSS_NODES = "${deployParams.enforce_replicas_across_nodes}"
+        //   UPDATE_STRATEGY = "${deployParams.update_strategy}"
+        //   CLUSTERED = "${deployParams.clustered}"
+        //   FORWARD_SSL_SESSION = "${deployParams.forward_ssl_session}"
+        //   LAST_MILE_SECURITY = "${deployParams.last_mile_security}"
+        //   PERSISTENT_OBJECT_STORE = "${deployParams.persistent_object_store}"                  
 
-          APP_NAME = "${PORTFOLIO_NAME_LOWER}-${PROJECT}-${ANYPOINT_ENV}"           
-        }
+        //   APP_NAME = "${PORTFOLIO_NAME_LOWER}-${PROJECT}-${ANYPOINT_ENV}"           
+        // }
         steps {
           script {
             //def appName = "${PORTFOLIO_NAME_LOWER}-${pipelineParams.projectName}-${ANYPOINT_ENV}"
-            println "App Name: ${APP_NAME}"
+            //println "App Name: ${APP_NAME}"
 
-            // def deployParams = mwDefaults.deployment_Params_Defaults
+            def mule-env = "dev1"
+            def anypoint-env = mule-env
 
-            // println "deployParams1: ${deployParams}"
+            def deployParams = mwDefaults.deployment_Params_Defaults
 
-            // deployUtils.getDeploymentConfigs(deployParams,"${PORTFOLIO_NAME_LOWER}","${ANYPOINT_ENV}","${PROJECT}")
+            println "deployParams1: ${deployParams}"
 
-            // println "deployParams2: ${deployParams}"
+            deployUtils.getDeploymentConfigs(deployParams,"${PORTFOLIO_NAME_LOWER}",anypoint-env,"${PROJECT}")
+
+            println "deployParams2: ${deployParams}"
 
             def clusters = deployParams['CLUSTERS']
             //def mvnArgs = "${mwDefaults.mvnArgs}"
@@ -136,17 +139,36 @@ def call(Map pipelineParams) {
             //println "RTF Cluster:  ${RTF_CLUSTER_NAME}"
 
             //println "Cluster: " + cluster
-            
-            withCredentials([
-              usernamePassword(credentialsId: "${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-creds", usernameVariable: 'ap_user', passwordVariable: 'ap_pass'),
-              string(credentialsId: "${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-key", variable: 'key')
-              ]) {     
-                for (cluster in clusters)   { 
-                  println "Deploying to Cluster: " + cluster
-                  //sh 'mvn $MVN_ARGS -Prtf mule:deploy -Dmule.artifact=dummy.jar -Danypoint.env.clientId=$ap_user -Danypoint.env.clientSecret=$ap_pass -Dsecret.key=$key -Drtf.cluster=$cluster'
+            withEnv ([
+                MULE_ENV = "${mule-env}",
+                ANYPOINT_ENV = "${anypoint-env}",
+                APP_NAME = "${PORTFOLIO_NAME_LOWER}-${anypoint-env}-${PROJECT}"
+                CPU_RESERVED = "${deployParams.cpu_reserved}",
+                CPU_LIMIT = "${deployParams.cpu_limit}",
+                MEMORY_RESERVED = "${deployParams.memory_reserved}",
+                REPLICAS = "${deployParams.replicas}",
+
+                MULE_VERSION = "${deployParams.mule_version}",
+                RTF_PROVIDER = "${deployParams.provider}",
+                SKIP_DEPLOY_VERIFY = "${deployParams.skip_deploy_verify}",
+
+                ENFORCE_REPLICAS_ACROSS_NODES = "${deployParams.enforce_replicas_across_nodes}",
+                UPDATE_STRATEGY = "${deployParams.update_strategy}",
+                CLUSTERED = "${deployParams.clustered}",
+                FORWARD_SSL_SESSION = "${deployParams.forward_ssl_session}",
+                LAST_MILE_SECURITY = "${deployParams.last_mile_security}",
+                PERSISTENT_OBJECT_STORE = "${deployParams.persistent_object_store}",
+            ]) {
+                  withCredentials([
+                    usernamePassword(credentialsId: "${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-creds", usernameVariable: 'ap_user', passwordVariable: 'ap_pass'),
+                    string(credentialsId: "${PORTFOLIO_NAME_LOWER}-${MULE_ENV}-key", variable: 'key')
+                    ]) {     
+                      for (cluster in clusters)   { 
+                        println "Deploying to Cluster: " + cluster
+                        //sh 'mvn $MVN_ARGS -Prtf mule:deploy -Dmule.artifact=dummy.jar -Danypoint.env.clientId=$ap_user -Danypoint.env.clientSecret=$ap_pass -Dsecret.key=$key -Drtf.cluster=$cluster'
+                      }
+                  }                     
                 }
-            }                     
-          }
         }
       }
 
